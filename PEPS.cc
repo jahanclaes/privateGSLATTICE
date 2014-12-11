b8126ac0c7a011fd1843e0dce9c5dffc048f4566
Bryan Clark <bryanc@h2ologin3.ncsa.illinois.edu>
2014-12-11 11:53:06 -0600

    Added PEPS files

---
+++ PEPS.cc
@@ -0,0 +1,157 @@
+#include "PEPS.h"
+#include "SmartPfaffian.h"
+
+void 
+PEPSClass::AllDerivs(SystemClass &system, Array<complex<double>,1> &derivs)
+{
+  AllDerivs(system,derivs,0,derivs.size());
+}
+
+void 
+PEPSClass::AllDerivs(SystemClass &system, Array<complex<double>,1>  &derivs,int start, int stop)
+{
+  for (int i=start;i<stop;i++)
+    derivs(i)=0.0;
+
+  for (int i=0;i<PF.NumCorrelators;i++){
+    int theBin=PF.corr2Bin(system.x,i);
+    assert(start+PF.binLoc[i][theBin]<derivs.size());
+    derivs(start+PF.binLoc[i][theBin])+=1.0/(PF.corr2Val(system.x,i)); // f0[PairingFunction.FindBin(r)].real());
+
+  }
+
+  return;
+}
+
+
+void 
+PEPSClass::RealDerivs(SystemClass &system, Array<complex<double>,1> &derivs)
+{
+  int start=0;
+  int stop=derivs.size();
+  for (int i=start;i<stop;i++)
+    derivs(i)=0.0;
+  for (int j=0;j<PF.NumCorrelators;j++){
+    int theBin=PF.corr2Bin(system.x,j);
+    double total=1.0;
+    for (int i=0;i<PF.NumCorrelators;i++){
+      if (i!=theBin)
+	total*=PF.corr2Val(system.x,i).real();
+    }
+    derivs(start+PF.binLoc[j][theBin])+=total;
+  }
+
+}
+
+
+
+
+
+void 
+PEPSClass::CheckDerivs(SystemClass &system, Array<complex<double>,1>  &derivs,int start, int stop)
+{
+  Array<double,1> check_derivs(derivs.size());
+  for (int i=start;i<stop;i++)
+    check_derivs(i)=0.0;
+  double eps=1e-10;
+  for (int i=0;i<PF.NumCorrelators;i++){
+    int theBin=PF.corr2Bin(system.x,i);
+    PF.f0[i][theBin]+=eps;
+    complex<double> up=evaluate(system);
+    PF.f0[i][theBin]-=2*eps;
+    complex<double> down=evaluate(system);
+    PF.f0[i][theBin]+=eps;
+    complex<double> thestart=evaluate(system);
+    assert(start+PF.binLoc[i][theBin]<stop);
+    check_derivs(start+PF.binLoc[i][theBin])=(up.real()-down.real())/(2*eps*thestart.real());
+
+    //    derivs(start+PF.binLoc[i][theBin])+=1.0/(PF.corr2Val(system.x,i)); // f0[PairingFunction.FindBin(r)].real());
+  }
+  for (int i=start;i<stop;i++)
+    cerr<<"DERIV CHECK: "<<derivs(i).real()<<" "<<check_derivs(i)<<endl;
+
+  return;
+}
+
+
+
+complex<double> 
+PEPSClass::evaluate(SystemClass &system)
+{
+  double total=1.0;
+  for (int i=0;i<PF.NumCorrelators;i++){
+    total*=PF.corr2Val(system.x,i).real();
+  }
+  return total;
+}
+
+complex<double> 
+PEPSClass::evaluateRatio(SystemClass &system,int start, int stop, int spin)
+{
+  //  complex<double> toCheck=evaluateRatio_check(system,swap1,swap2);
+//   //  cerr<<"ENDTERING"<<endl;
+  double ratio=1.0;
+  ///for each particle you need to know all the correlators you've upset
+  //UNCOMMENT ME  double newVal=f(system.x);
+  system.Move(start,stop,spin);
+  //UNCOMMENT ME  double oldVal=f(system.x);
+  system.Move(start,stop,spin);
+  ratio*=(newVal/oldVal);
+  return ratio;
+  
+}
+
+//assumes x(swap1) != x(swap2)
+//When swap1 and swap2 exchange, the term that corresponds to them
+//doesn't change at all. 
+//Called after the particles have been swapped!
+complex<double> 
+PEPSClass::evaluateRatio(SystemClass &system, int swap1, int swap2)
+{
+  //  complex<double> toCheck=evaluateRatio_check(system,swap1,swap2);
+  //  cerr<<"ENDTERING"<<endl;
+  double ratio=1.0;
+  ///for each particle you need to know all the correlators you've upset
+  set<int> correlatorsChanged;
+  correlatorsChanged.insert(PF.correlatorsForSite[swap1].begin(),PF.correlatorsForSite[swap1].end());
+  correlatorsChanged.insert(PF.correlatorsForSite[swap2].begin(),PF.correlatorsForSite[swap2].end());
+  //  cerr<<"PRE FOR LOOP"<<endl;
+  for (set<int>::iterator corrIter = correlatorsChanged.begin();corrIter!=correlatorsChanged.end();corrIter++){
+    int corr=*corrIter;
+    double newVal=PF.corr2Val(system.x,corr).real();
+    //    cerr<<"swap1 swap2 "<<swap1<<" "<<swap2<<" "<<corr<<endl;
+    //    for (int k=0;k<PF.myCorrs[corr].size();k++)
+    //      cerr<<PF.myCorrs[corr][k]<<endl;
+    //    cerr<<"newVal is "<<newVal<<endl;
+    system.Swap(swap1,swap2);
+    double oldVal=PF.corr2Val(system.x,corr).real();
+    system.Swap(swap1,swap2);
+    ratio*=(newVal/oldVal);
+  }
+  //  cerr<<"POST FOR LOOP"<<endl;
+  //  cerr<<"CHECKING: "<<ratio<<" "<<toCheck<<endl;
+  return ratio;
+}
+
+
+complex<double> 
+PEPSClass::logevaluate(SystemClass &system,int &sign)
+{
+  complex<double> val = evaluate(system);
+  sign= val.real() >0 ? 1: -1;
+  return log(abs(val.real()));
+}
+
+
+complex<double> 
+PEPSClass::evaluateRatio_check(SystemClass &system, int swap1, int swap2)
+{
+  system.Swap(swap1,swap2);
+  int signa;
+  complex<double> before=logevaluate(system,signa);
+  system.Swap(swap1,swap2);
+  int signb;
+  complex<double> after=logevaluate(system,signb);
+  return exp(after.real()-before.real())*signa*signb;
+}
+
---
+++ PEPS.h
@@ -0,0 +1,99 @@
+#ifndef PEPS_H
+#define PEPS_H
+#include <iostream>
+#include <algorithm>
+#include <fstream>
+#include <vector>
+
+#include "Random/Random.h"
+#include "Blitz.h"
+#include "SystemClass.h"
+#include "WaveFunction.h"
+#include "PairingFunctionMany.h"
+
+class PEPSClass  : public WaveFunctionClass
+{
+public:
+  int NumSpinUp;
+  PEPSClass(PairingFunctionMany &t_PF) : PF(t_PF)
+  {
+  }
+
+  complex<double> logevaluate(SystemClass &system,int &sign);
+
+  //currently only the size matters
+  void Swap(int i, int j)
+  {
+  }
+
+  void Move(int site, int end_site, int spin)
+  {
+    
+  }
+  void Reject(SystemClass &system,int site,int end_site,int spin)
+  {
+    
+  }
+
+
+  void AllDerivs(SystemClass &system, Array<complex<double>,1>  &derivs);
+  void AllDerivs(SystemClass &system, Array<complex<double>,1> &derivs,int start,int stop);
+  void RealDerivs(SystemClass &system, Array<complex<double>,1> &derivs)  ;
+
+
+  void  CheckDerivs(SystemClass &system, Array<complex<double>,1>  &derivs,int start, int stop);
+    
+  void Init(SystemClass &system)
+  {
+    Name="PEPS";
+    NeedFrequentReset=false;
+    NumSpinUp=system.x.size()/2;
+    PF.ProcessSystem(system);
+
+    bool ReadParams=false;
+    if (ReadParams){
+    }
+    NumParams=PF.NumParams; //Correlators;
+  }
+  double GetParam_real(int i)
+  {
+    pair<int,int> myLoc=PF.ParamLoc[i];
+    return PF.f0[myLoc.first][myLoc.second].real();
+  }
+  void SetParam_real(int i,double param)
+  {
+    pair<int,int> myLoc=PF.ParamLoc[i];
+    PF.f0[myLoc.first][myLoc.second].real()=param;
+  }
+
+  double GetParam_imag(int i)
+  {
+    pair<int,int> myLoc=PF.ParamLoc[i];
+    return PF.f0[myLoc.first][myLoc.second].imag();
+  }
+  void SetParam_imag(int i,double param)
+  {
+    pair<int,int> myLoc=PF.ParamLoc[i];
+    PF.f0[myLoc.first][myLoc.second].imag()=param;
+  }
+
+
+  complex<double> evaluateRatio(SystemClass &system,int start, int stop, int spin);
+  
+
+  complex<double> evaluate(SystemClass &system);
+  complex<double> evaluateRatio(SystemClass &system,int swap1, int swap2);
+  complex<double> evaluateRatio_check(SystemClass &system, int swap1, int swap2);
+  void UpdateDets(SystemClass &system,int swap1, int swap2)
+  {
+  }
+
+  void UpdateDets(SystemClass &system,int site, int end_site,int spin)
+  {
+
+  }
+
+
+};
+
+#endif
